91021678d457f64a48cfd4b1193952ee6e470804
Jeffrey Massung <massung@gmail.com>
2019-11-23 11:35:16 -0600

    Fixed production server continuations.

--- run-server.sh
+++ run-server.sh
@@ -1,2 +1,6 @@
 #!/usr/bin/env bash
-gunicorn -b '127.0.0.1:5000' -w 4 server:app
+
+# NOTE: Don't ever use more than 1 worker as the continuation tokens are only
+#       valid on the worker that created them. Instead, use multiple threads.
+
+gunicorn -b '127.0.0.1:5000' -k sync -w 1 --threads 4 server:app
