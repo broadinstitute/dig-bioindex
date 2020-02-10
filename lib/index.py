786e75dec088044cd7a521d5aeee1a8ffec4c5d0
Jeffrey Massung <massung@gmail.com>
2020-02-10 09:35:29 -0600

    Shared code for indexing.

--- lib/index.py
+++ lib/index.py
@@ -12,42 +12,62 @@ import lib.schema
 def build(engine, table, schema, bucket, s3_objects):
     """
     Build an index table for a set of objects in an S3 bucket using a schema.
+    The schema is a string that is either a locus of column names or a single
+    field. Example schemas:
+
+      * chromosome:position
+      * chromosome:start-end
+      * phenotype
+      * varId
     """
     locus_class, locus_cols = lib.locus.parse_columns(schema)
 
     if locus_class:
-        return _by_locus(engine, table, schema, bucket, s3_objects)
+        return _index_objects(
+            engine,
+            table,
+            schema,
+            bucket,
+            s3_objects,
+            lambda row: locus_class(*(row.get(col) for col in locus_cols if col)).loci(),
+            lambda k: {'chromosome': k[0], 'position': k[1]},
+        )
     else:
-        return _by_value(engine, table, schema, bucket, s3_objects)
+        return _index_objects(
+            engine,
+            table,
+            schema,
+            bucket,
+            s3_objects,
+            lambda row: [row[schema]],
+            lambda k: {'value': k},
+        )
 
 
-def _by_locus(engine, table, locus, bucket, s3_objects):
+def _index_objects(engine, table, schema, bucket, s3_objects, keys, index_keys):
     """
-    Index table records in s3 to redis.
+    Builds the index table for objects in S3.
     """
-    locus_class, locus_cols = lib.locus.parse_columns(locus)
+    lib.metadata.update(engine, table.name, schema)
 
-    # create the metadata table if it doesn't exist yet
-    lib.metadata.update(engine, table.name, locus)
-
-    # drop and create the table
+    # create the index table (drop any existing table already there)
     logging.info('Creating %s table...', table.name)
     table.drop(engine, checkfirst=True)
     table.create(engine)
 
-    # collect all the s3 objects
+    # collect all the s3 objects into a list so the size is known
     objects = list(s3_objects)
 
     # progress bar management
     with enlighten.get_manager() as progress_mgr:
         overall_progress = progress_mgr.counter(total=len(objects), unit='files')
 
-        # list all the input tables
+        # process each s3 object
         for obj in objects:
-            path, size, tag = obj['Key'], obj['Size'], obj['ETag']
+            path, size = obj['Key'], obj['Size']
             logging.info('Processing %s...', path)
 
-            # create line progress bar
+            # create progress bar for each file
             file_progress = progress_mgr.counter(total=size // 1024, unit='KB', leave=False)
 
             # stream the file from s3
@@ -55,20 +75,17 @@ def _by_locus(engine, table, locus, bucket, s3_objects):
             start_offset = 0
             records = {}
 
-            # process the records
+            # process each line (record)
             for line_num, line in enumerate(content.iter_lines()):
                 row = json.loads(line)
                 end_offset = start_offset + len(line) + 1  # newline
 
                 try:
-                    locus_obj = locus_class(*(row.get(col) for col in locus_cols if col))
-
-                    # add new loci or expand existing
-                    for locus in locus_obj.loci():
-                        if locus in records:
-                            records[locus]['end_offset'] = end_offset
+                    for k in keys(row):
+                        if k in records:
+                            records[k]['end_offset'] = end_offset
                         else:
-                            records[locus] = {
+                            records[k] = {
                                 'path': path,
                                 'start_offset': start_offset,
                                 'end_offset': end_offset,
@@ -78,19 +95,21 @@ def _by_locus(engine, table, locus, bucket, s3_objects):
                     logging.warning('%s; skipping...', e)
 
                 # update the progress bar
-                file_progress.update(incr=(end_offset-start_offset) // 1024)
+                file_progress.update(incr=(end_offset // 1024) - file_progress.count)
 
                 # track current file offset
                 start_offset = end_offset
 
-            # transform all the records
-            batch = [{'chromosome': locus[0], 'position': locus[1], **r} for locus, r in records.items()]
+            # done processing the file
+            file_progress.close()
+
+            # transform all the records and collect them all into an insert batch
+            batch = [{**index_keys(k), **r} for k, r in records.items()]
 
             # perform the insert in batches
             _batch_insert(engine, table, batch, progress_mgr=progress_mgr)
 
-            # done processing the file
-            file_progress.close()
+            # update progress
             overall_progress.update()
 
         # show the number of records attempting to be inserted
@@ -98,82 +117,6 @@ def _by_locus(engine, table, locus, bucket, s3_objects):
 
         # finally, build the index after all inserts are done
         sqlalchemy.Index('locus_idx', table.c.chromosome, table.c.position).create(engine)
-
-        # done
-        overall_progress.close()
-
-
-def _by_value(engine, table, column, bucket, s3_objects):
-    """
-    Index table records in s3 to redis.
-    """
-    lib.metadata.update(engine, table.name, column)
-
-    # drop and create the table
-    logging.info('Creating %s table...', table.name)
-    table.drop(engine, checkfirst=True)
-    table.create(engine)
-
-    # collect all the s3 objects
-    objects = list(s3_objects)
-
-    # progress bar management
-    with enlighten.get_manager() as progress_mgr:
-        overall_progress = progress_mgr.counter(total=len(objects), unit='files')
-
-        # list all the input tables
-        for obj in objects:
-            path, size, tag = obj['Key'], obj['Size'], obj['ETag']
-            logging.info('Processing %s...', path)
-
-            # create line progress bar
-            file_progress = progress_mgr.counter(total=size // 1024, unit='KB', leave=False)
-
-            # stream the file from s3
-            content = lib.s3.read_object(bucket, path)
-            start_offset = 0
-            records = {}
-
-            # process the records
-            for line_num, line in enumerate(content.iter_lines()):
-                row = json.loads(line)
-                end_offset = start_offset + len(line) + 1  # newline
-
-                try:
-                    value = row[column]
-
-                    # add new value or expand
-                    if value in records:
-                        records[value]['end_offset'] = end_offset
-                    else:
-                        records[value] = {
-                            'path': path,
-                            'start_offset': start_offset,
-                            'end_offset': end_offset,
-                            'value': value,
-                        }
-
-                except KeyError as e:
-                    logging.warning('%s; skipping...', e)
-
-                # update the progress bar
-                file_progress.update(incr=(end_offset-start_offset) // 1024)
-
-                # track current file offset
-                start_offset = end_offset
-
-            # perform the insert in batches
-            _batch_insert(engine, table, list(records.values()), progress_mgr=progress_mgr)
-
-            # done processing the file
-            file_progress.close()
-            overall_progress.update()
-
-        # show the number of records attempting to be inserted
-        logging.info('Building table index...')
-
-        # finally, build the index after all inserts are done
-        sqlalchemy.Index('locus_idx', table.c.value).create(engine)
 
         # done
         overall_progress.close()
