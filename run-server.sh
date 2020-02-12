bb1073e32481e63b6f97178784c5158e4feac90a
Jeffrey Massung <massung@gmail.com>
2020-02-12 15:16:19 -0600

    Bind to 0.0.0.0 for the server.

--- run-server.sh
+++ run-server.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 # NOTE: Always use 1 worker per thread so that continuations work!
-gunicorn -b '127.0.0.1:5000' -k sync -w 1 --threads 4 server:app
+gunicorn -b '0.0.0.0:5000' -k sync -w 1 --threads 4 server:app
