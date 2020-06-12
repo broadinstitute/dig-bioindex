70b61c84bddfe6d61fcc085021d9614d240c7807
Jeffrey Massung <massung@gmail.com>
2020-06-12 09:06:34 -0500

    Disease groups are optionally part of CMD.

--- api/portal.py
+++ api/portal.py
@@ -23,7 +23,7 @@ async def api_portal_groups():
     Returns the list of portals available.
     """
     sql = (
-        'SELECT DISTINCT `name`, `description`, `default`, `docs` FROM DiseaseGroups'
+        'SELECT DISTINCT `name`, `description`, `default`, `memberCMD` FROM DiseaseGroups'
     )
 
     # run the query
@@ -31,12 +31,12 @@ async def api_portal_groups():
     disease_groups = []
 
     # transform response
-    for name, desc, default, docs in resp:
+    for name, desc, default, member_cmd in resp:
         disease_groups.append({
             'name': name,
             'default': default != 0,
             'description': desc,
-            'docs': docs,
+            'memberCMD': member_cmd != 0,
         })
 
     return {
@@ -53,31 +53,28 @@ async def api_portal_groups():
 async def api_group_phenotypes(q: str = None):
     """
     Returns all available phenotypes or just those for a given
-    portal group.
+    disease group.
     """
-    sql = (
-        'SELECT '
-        '   Phenotypes.`name`, '
-        '   Phenotypes.`description`, '
-        '   Phenotypes.`group`, '
-        '   Phenotypes.`dichotomous` '
-        'FROM Phenotypes'
-    )
+    sql = 'SELECT `name`, `description`, `group`, `dichotomous` FROM Phenotypes'
 
-    # optional filter by portal
-    if q and q == '':
-        q = None
+    # groups to match
+    groups = None
 
-    # update query for just the portal
-    if q:
-        sql += (
-            ', DiseaseGroups WHERE DiseaseGroups.`name` = %s AND ('
-            '   DiseaseGroups.`groups` IS NULL OR FIND_IN_SET(Phenotypes.`group`, DiseaseGroups.`groups`) '
-            ')'
-        )
+    # optionally filter by disease group
+    if q and q != '':
+        resp = engine.execute('SELECT `groups` FROM DiseaseGroups WHERE `name` = %s', q)
+        groups = resp.fetchone()
+
+        # groups are a comma-separated set
+        if groups:
+            groups = groups[0] and groups[0].split(',')
+
+    # collect phenotype groups by union
+    if groups is not None:
+        sql = ' UNION '.join(f'({sql} WHERE FIND_IN_SET(%s, Phenotypes.`group`))' for _ in groups)
 
     # run the query
-    resp, query_s = profile(engine.execute, sql, q) if q else profile(engine.execute, sql)
+    resp, query_s = profile(engine.execute, sql, *groups) if groups else profile(engine.execute, sql)
     phenotypes = []
 
     # transform response
