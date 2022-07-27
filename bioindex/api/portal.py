26f69147c225ebfb1f694d1b51e5b27d45667249
psmadbec <psmadbec@broadinstitute.org>
2022-07-27 12:15:13 -0400

    [Bioindex] Add in phenotype to systems endpoint

--- bioindex/api/portal.py
+++ bioindex/api/portal.py
@@ -265,14 +265,17 @@ async def api_portal_systems(req: fastapi.Request):
 
     # fetch all systems, join to diseases and phenotype groups
     sql = (
-        'SELECT s.system, '
-        '       d.disease, '
-        '       g.group '
-        'FROM SystemToDisease stod '
-        'JOIN DiseaseToGroup dtog ON stod.diseaseId = dtog.diseaseId '
-        'JOIN Systems s ON s.id = stod.systemId '
-        'JOIN Diseases d ON d.id = stod.diseaseId '
-        'JOIN PhenotypeGroups g ON g.id = dtog.groupId'
+        """
+        SELECT s.system, d.disease, g.group, p.name as phenotype
+            FROM SystemToDisease stod
+            JOIN DiseaseToGroup dtog ON stod.diseaseId = dtog.diseaseId
+            JOIN GroupToPhenotype gtop ON dtog.groupId = gtop.groupId
+            JOIN Systems s ON s.id = stod.systemId
+            JOIN Diseases d ON d.id = stod.diseaseId
+            JOIN PhenotypeGroups g ON g.id = dtog.groupId
+            JOIN Phenotypes p ON p.id = gtop.phenotypeId
+        ORDER BY s.system, d.disease, g.group, p.name
+        """
     )
 
     # get all datasets
@@ -284,7 +287,8 @@ async def api_portal_systems(req: fastapi.Request):
         system = {
             'system': r[0],
             'disease': r[1],
-            'group': r[2]
+            'group': r[2],
+            'phenotype': r[3]
         }
 
         systems.append(system)
