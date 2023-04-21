5aac5013037c40d0d7bd33a9b20fa4214094b04e
Drew Hite <drew@drewhite.org>
2023-04-21 14:09:10 -0400

    use click for params

--- batch-index-files/main.py
+++ batch-index-files/main.py
@@ -2,20 +2,23 @@ import sys
 import boto3
 import subprocess
 import concurrent.futures
+import click
 
 
-def main(argv):
+@click.command()
+@click.option('--bucket', '-b', type=str)
+@click.option('--path', '-p', type=str)
+def main(bucket, path):
     s3 = boto3.client('s3')
 
-    bucket_name = 'dig-bio-index'
-    response = s3.list_objects(Bucket=bucket_name, Prefix=argv[0])
+    response = s3.list_objects(Bucket=bucket, Prefix=path)
 
     with concurrent.futures.ThreadPoolExecutor(max_workers=4) as executor:
         futures = []
         for file in response['Contents']:
             if not str(file['Key']).endswith(".bgz"):
                 continue
-            futures.append(executor.submit(bg_index_file, bucket_name, file))
+            futures.append(executor.submit(bg_index_file, bucket, file))
 
         # get the results of the tasks as they complete
         for future in concurrent.futures.as_completed(futures):
