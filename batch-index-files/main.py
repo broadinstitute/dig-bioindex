33b7709902d377abe638133399a33d9a8be5aca4
Drew Hite <drew@drewhite.org>
2023-04-26 11:58:52 -0400

    Revert "use click for params"
    This reverts commit 5aac5013037c40d0d7bd33a9b20fa4214094b04e.

--- batch-index-files/main.py
+++ batch-index-files/main.py
@@ -2,23 +2,20 @@ import sys
 import boto3
 import subprocess
 import concurrent.futures
-import click
 
 
-@click.command()
-@click.option('--bucket', '-b', type=str)
-@click.option('--path', '-p', type=str)
-def main(bucket, path):
+def main(argv):
     s3 = boto3.client('s3')
 
-    response = s3.list_objects(Bucket=bucket, Prefix=path)
+    bucket_name = 'dig-bio-index'
+    response = s3.list_objects(Bucket=bucket_name, Prefix=argv[0])
 
     with concurrent.futures.ThreadPoolExecutor(max_workers=4) as executor:
         futures = []
         for file in response['Contents']:
             if not str(file['Key']).endswith(".bgz"):
                 continue
-            futures.append(executor.submit(bg_index_file, bucket, file))
+            futures.append(executor.submit(bg_index_file, bucket_name, file))
 
         # get the results of the tasks as they complete
         for future in concurrent.futures.as_completed(futures):
