8e92d73d5d0c42b60f4a2f432184c0585b1dfac3
Drew Hite <drew@drewhite.org>
2024-04-12 16:19:00 -0400

    handle gzip properly

--- bioindex/api/raw.py
+++ bioindex/api/raw.py
@@ -76,8 +76,11 @@ async def api_raw_file(file: str, req: fastapi.Request):
     content = s3.read_object(CONFIG.s3_bucket, f'raw/{file}')
     if content is None:
         raise fastapi.HTTPException(status_code=404)
-    content_type, _ = mimetypes.guess_type(file)
-    if content_type is None:
+    content_type, encoding = mimetypes.guess_type(file)
+    if encoding == 'gzip':
+        content_type = 'application/gzip'
+    elif content_type is None:
         content_type = 'application/octet-stream'
+
 
     return fastapi.Response(content=content.read(), media_type=content_type)
