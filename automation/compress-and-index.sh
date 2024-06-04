6c31d3326f946086048011a51908873ccfc32fed
psmadbec <psmadbec@broadinstitute.org>
2024-06-04 16:19:03 -0400

    stash

--- automation/compress-and-index.sh
+++ automation/compress-and-index.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-python3.8 -m bioindex.main compress dataset-associations associations/dataset/
-python3.8 -m bioindex.main remove-uncompressed-files dataset-associations associations/dataset/
-yes | python3.8 -m bioindex.main index dataset-associations -l -w 30
+index_name=$1
+index_path=$2
+
+python3.8 -m bioindex.main compress $index_name $index_path
+python3.8 -m bioindex.main remove-uncompressed-files $index_name $index_path
+yes | python3.8 -m bioindex.main index $index_name -l -w 30
