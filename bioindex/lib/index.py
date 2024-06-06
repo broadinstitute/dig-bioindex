e74d971a52ffb4b5a56a991056cab71cc272f99d
psmadbec <psmadbec@broadinstitute.org>
2024-06-06 15:26:35 -0400

    stash

--- bioindex/lib/index.py
+++ bioindex/lib/index.py
@@ -37,14 +37,13 @@ class Index:
 
     @staticmethod
     def set_compressed(engine, name, prefix, compressed):
-        print(name, prefix, compressed)
         with engine.connect() as conn:
             conn.execute(
                 sqlalchemy.text(
                     'UPDATE `__Indexes` SET compressed = :compressed '
                     'WHERE `name` = :name and prefix = :prefix'
                 ),
-                {'name': name, 'prefix': prefix, 'compressed': 1 if compressed else 0}
+                {'name': name, 'prefix': prefix, 'compressed': compressed}
             )
 
     @staticmethod
