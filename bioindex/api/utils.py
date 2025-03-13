51ea9d12c18e7c43ca190869137cb3679984e92a
psmadbec <psmadbec@broadinstitute.org>
2025-03-13 10:26:57 -0400

    stash

--- bioindex/api/utils.py
+++ bioindex/api/utils.py
@@ -26,4 +26,4 @@ def connect_to_portal(config):
     The portal/metadata schema is completely optional.
     """
     if config.portal_schema:
-        return aws.connect_to_db(**config.rds_config, schema=config.portal_schema)
+        return aws.connect_to_db(**config.portal_rds_secret, schema=config.portal_schema)
