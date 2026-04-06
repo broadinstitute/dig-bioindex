09ce730b10ff6a57eac33bfb804b6d0d214e96b8
Drew Hite <drew@drewhite.org>
2026-04-06 18:05:12 -0400

    configurable workers

--- bioindex/main.py
+++ bioindex/main.py
@@ -65,11 +65,13 @@ SERVER_LOGGING_CONFIG = {
 
 @click.command(name='serve')
 @click.option('--port', '-p', type=int, default=5000)
-def cli_serve(port):
+@click.option('--workers', '-w', type=int, default=1)
+def cli_serve(port, workers):
     uvicorn.run(
         'bioindex.server:app',
         host='0.0.0.0',
         port=port,
+        workers=workers,
         log_level='info',
         log_config=SERVER_LOGGING_CONFIG
     )
